3c18649811d9c80ea92f87cd666a1f79464e759d
olga-maev <kurumoch0@gmail.com>
2022-10-02 15:30:20 +0400

    First commit nariables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
+'use strict'
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+  let d = b**2-4*a*c
+  if (d<0){ 
+    arr=[]
+  }
+  if (d==0){
+    arr=[-b/(2*a)]
+  } 
+  if (d>0) {
+    arr=[(-b+Math.sqrt(d))/(2*a),(-b-Math.sqrt(d))/(2*a)]
+  }
   return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
+  let s=amount-contribution
+  percent=percent/100
+  let countMonth=Math.round((date - Date.now())/(29.5*24*60*60*1000))
+  let payMonth=s*(percent+(percent/(((1+percent)**countMonth)-1)))
+  totalAmount=parseFloat((Math.round(payMonth*100)/10).toFixed(2))
+ 
   return totalAmount;
 }
