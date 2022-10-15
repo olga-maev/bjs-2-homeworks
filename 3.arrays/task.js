e29c98a2bbb55dc8c43fa3fbb753fbb19a456e4a
olga-maev <kurumoch0@gmail.com>
2022-10-16 02:37:51 +0400

    First commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,23 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length !== arr2.length) result=false
+  else{
+    result=true;
+    for (let counter=0;counter<arr1.length;counter++){
+      if (arr1[counter]!==arr2[counter]) {
+        result = false;
+        break;
+      }
+    }
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr=arr.filter(count => count%3===0)
+  resultArr=resultArr.filter(count => count>0)
+  resultArr=resultArr.map((count)=>count*10)
+  
   return resultArr; // array
 }
