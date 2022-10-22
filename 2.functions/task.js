4710c62956dbac13363c470c2a1ceee0ff419e65
olga-maev <kurumoch0@gmail.com>
2022-10-22 19:54:44 +0400

    Second commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,46 +1,40 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-  min=101
-  max=-101
-  sum=0
-  for (let counter=0;counter<arr.length;counter++){
-    if (arr[counter]>max) max=arr[counter]
-    if (arr[counter]<min) min=arr[counter]
-    sum+=arr[counter]
+  let avg;
+  let min = max = arr[0]
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) max = arr[i]
+    if (arr[i] < min) min = arr[i]
+    sum += arr[i]
   }
-  avg=Number((sum/arr.length).toFixed(2))
+  avg = Number((sum / arr.length).toFixed(2))
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-  sum=0
-  for (let counter=0;counter<arr.length;counter++) {
-    sum+=arr[counter]
+  let sum = 0
+  for (let counter = 0; counter < arr.length; counter++) {
+    sum += arr[counter]
   }
   return sum;
 }
 
-function makeWork(arrOfArr,func) {
-  let max;
-  max=func(arrOfArr[0])
-  for (counter=1;counter<arrOfArr.length;counter++){
-    if (func(arrOfArr[counter])>max) max=func(arrOfArr[counter])
+function makeWork(arrOfArr, func) {
+  let max = func(arrOfArr[0])
+  for (let i = 1; i < arrOfArr.length; i++) {
+    if (func(arrOfArr[i]) > max) max = func(arrOfArr[i])
   }
-    return max;
+  return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  let max,min;
-  max=arr[0];
-  min=arr[0];
-  for (counter=1;counter<arr.length;counter++){
-    if (max<arr[counter]) max=arr[counter]
-    if (min>arr[counter]) min=arr[counter]
+  let max = min = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (max < arr[i]) max = arr[i]
+    if (min > arr[i]) min = arr[i]
   }
-  let rezult=max-min;
-  return rezult;
+  return max - min;
 }
