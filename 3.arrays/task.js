4c13f233ac30e2e371e6a4ca354096c2b8ce6909
olga-maev <kurumoch0@gmail.com>
2022-10-22 22:30:42 +0400

    Second commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,18 +1,13 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  if ((arr1.length !== arr2.length) & ( )){
-    result = true
-  }
-  else{
-    result=false
-      }
-    
+
+  let result = (arr1.length == arr2.length && arr1.every((v, i) => v === arr2[i]))
+
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-  resultArr=arr.filter(count => count%3===0).filter(count => count>0).map((count)=>count*10)
-  
+  resultArr = arr.filter(count => count % 3 === 0).filter(count => count > 0).map((count) => count * 10)
+
   return resultArr; // array
 }
