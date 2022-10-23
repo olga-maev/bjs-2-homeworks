bd86c755fc3d37142f35e94509fdd8dbba3abe9d
olga-maev <kurumoch0@gmail.com>
2022-10-23 21:34:22 +0400

    First commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,38 @@
+"use strict";
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Sergey", "male", 30);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark)
+  }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = [...marks];
+  } else {
+    this.marks.push(...marks)
+  }
+}
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce((accum, current) => accum + current, 0) / this.marks.length || 0;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
